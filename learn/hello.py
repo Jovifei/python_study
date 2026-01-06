c717942a0646896bc6a0ed2edf51180150b7b49d
JoviF <977191193@qq.com>
2026-01-06 22:40:36 +0800

    打印：print

--- learn/hello.py
+++ learn/hello.py
@@ -1,4 +1,34 @@
-print("hello world")
-print("hello world")
+# print("hello world")
+# print("hello world")
 
-print("hello world")
+# print("hello world")
+
+
+# dir(__builtins__) // 查看所有的内置函数
+
+# 幸运数 = 566
+# print(幸运数) //变量是字母数字向下滑线或者中文
+
+
+
+# xx=100
+# print(xx)
+# yy=200
+# print(yy)
+# xx, yy = yy, xx # 交换变量的值
+
+# print(xx, yy)
+
+
+
+# #都是对的，空格
+# xx=11
+# print(xx)
+# yy=22
+# print(yy)
+# xx,yy = yy,xx # 交换变量的值
+
+# print(xx,yy)
+
+print("x\'x,yy")
+print('2,ff')
